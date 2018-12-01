9ff155bfd771bf53fd513a8fcd84b306d499c49b
vimmmoos <m.falzari@student.rug.nl>
2018-12-01 17:04:14 +0000

    mutal editing

--- go/src/goWiki/goWiki.go
+++ go/src/goWiki/goWiki.go
@@ -4,54 +4,25 @@ import (
 	//"fmt"
 	"errors"
 	"github.com/microcosm-cc/bluemonday"
-	"golang.org/x/net/html"
 	"html/template"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"regexp"
-	"strings"
+	"sync"
 )
 
 // really easy example of read and write on a file
 // struct with a Title and a Body of a slice of byte
-// NOTE !!!! the fields in every struct must start with a capital letter!!!!!!
 
 type Page struct {
 	Title string
 	Body  template.HTML
 }
 
-func (p *Page) rmScript() error {
-	doc := html.NewTokenizer(strings.NewReader(string(p.Body)))
-	trim := false
-	nBody := ""
-	for {
-		tag := doc.Next()
-		switch tag {
-		case html.ErrorToken:
-			if err := doc.Err(); err != io.EOF {
-				return err
-			}
-			p.Body = template.HTML(nBody)
-			return nil
-		case html.SelfClosingTagToken, html.CommentToken:
-			fallthrough
-		case html.StartTagToken, html.EndTagToken:
-			tagName, _ := doc.TagName()
-
-			if string(tagName) == "script" {
-				trim = !trim
-			} else {
-				nBody += string(doc.Raw())
-			}
-		case html.TextToken:
-			if !trim {
-				nBody += string(doc.Raw())
-			}
-		}
-	}
+type Writable struct {
+	sync.Mutex
+	allow map[string]bool
 }
 
 // a save method of Page struct  which return an error or its nil value if
@@ -96,6 +67,21 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	errorHandel(err, w)
 }
 
+func makeStash(writable *Writable) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		title, err := getTitle(w, r)
+		if err != nil {
+			return
+		}
+		p, err := loadPage(title)
+		errorHandel(err, w)
+		writable.Lock()
+		writable.allow[p.Title] = true
+		writable.Unlock()
+		http.Redirect(w, r, "/view/"+title, http.StatusFound)
+	}
+}
+
 // look at exampleHttp if u need to remember what an handler do
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
@@ -108,30 +94,51 @@ func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	renderTemplate(w, "view", p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-	// try to load the relative page
-	p, err := loadPage(title)
-	if err != nil {
-		p = &Page{Title: title}
+func makeEdit(writable *Writable) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		title, err := getTitle(w, r)
+		if err != nil {
+			return
+		}
+		p, err := loadPage(title)
+		if err != nil {
+			p = &Page{Title: title}
+		}
+		writable.Lock()
+		if allow, ok := writable.allow[p.Title]; !ok {
+			writable.allow[p.Title] = false
+		} else if !allow {
+			http.NotFound(w, r)
+			writable.Unlock()
+			return
+		}
+		writable.allow[p.Title] = false
+		writable.Unlock()
+		renderTemplate(w, "edit", p)
 	}
-	// render the template
-	renderTemplate(w, "edit", p)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-	body := r.FormValue("body")
-	// creating the relative *Page
-	// NOTE we have to cast body cause FormValue return a type string
-	p := &Page{Title: title, Body: template.HTML(body)}
-	// use the method to save the *p
-	err := p.save()
-	errorHandel(err, w)
-	//redirect to the view relative page
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+func makeSave(writable *Writable) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		title, err := getTitle(w, r)
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+		body := r.FormValue("body")
+		p := &Page{Title: title, Body: template.HTML(body)}
+		err = p.save()
+		errorHandel(err, w)
+		writable.Lock()
+		writable.allow[p.Title] = true
+		writable.Unlock()
+		http.Redirect(w, r, "/view/"+title, http.StatusFound)
+
+	}
 }
 
 // the regular expression which will must be verified before doing anything
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+var validPath = regexp.MustCompile("^/(edit|save|view|stash)/([a-zA-Z0-9]+)$")
 
 // extract the title  from an URL but the URL structure of it must respect the
 // regular expression
@@ -168,9 +175,11 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/edit/", makeHandler(editHandler))
-	http.HandleFunc("/save/", makeHandler(saveHandler))
+	writable := &Writable{allow: make(map[string]bool)}
+	http.HandleFunc("/edit/", makeEdit(writable))
+	http.HandleFunc("/save/", makeSave(writable))
 	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/stash/", makeStash(writable))
 	http.HandleFunc("/", defaultHandler)
 	http.HandleFunc("/home", homeHandler)
 
